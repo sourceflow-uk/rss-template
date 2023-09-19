dc3bde543245c6fc9f04be008df9113e94baf15d
Sean Jermey <sean.jermey@sourceflow.co.uk>
2023-09-19 14:38:21 +0100

    blockquote component

--- src/components/BlockQuote/component.jsx
+++ src/components/BlockQuote/component.jsx
@@ -1,3 +1,71 @@
-export const BlockQuote = () => {
-  return <div>BlockQuote Component</div>;
+import PropTypes from "prop-types";
+import { Carousel, Col, Container, Row } from "react-bootstrap";
+import { chunk } from "lodash";
+import classes from "./styles.module.scss";
+import clsx from "classnames";
+
+const BlockQuoteCards = ({ className, items, visibleCount }) => (
+  <Row className={clsx(className, { "px-4": visibleCount > 1 })}>
+    {items.map(({ quote, author }) => (
+      <Col>
+        <blockquote className={clsx(classes.blockquotes__card, "p-3")}>
+          <section className="mb-4" dangerouslySetInnerHTML={{ __html: quote }} />
+          <cite>
+            <b dangerouslySetInnerHTML={{ __html: author.name }} />
+            <br />
+            <span dangerouslySetInnerHTML={{ __html: author.position }} />
+          </cite>
+        </blockquote>
+      </Col>
+    ))}
+  </Row>
+);
+
+export const BlockQuote = ({ className, title, items, carousel, visibleCount }) => {
+  return (
+    <div className={clsx(className, classes.blockquotes)}>
+      <Container>
+        <h2>{title}</h2>
+        {carousel ? (
+          <Carousel className={classes.blockquotes__carousel} controls={true} indicators={false}>
+            {chunk(items, visibleCount).map((items) => (
+              <Carousel.Item>
+                <BlockQuoteCards items={items} visibleCount={visibleCount} />
+              </Carousel.Item>
+            ))}
+          </Carousel>
+        ) : (
+          <div className="px-5">
+            {chunk(items, visibleCount).map((items) => (
+              <BlockQuoteCards items={items} visibleCount={visibleCount} />
+            ))}
+          </div>
+        )}
+      </Container>
+    </div>
+  );
+};
+
+BlockQuote.defaultProps = {
+  className: "",
+  title: "",
+  items: [],
+  carousel: false,
+  visibleCount: 1,
+};
+
+BlockQuote.propTypes = {
+  className: PropTypes.string,
+  title: PropTypes.string,
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      quote: PropTypes.string,
+      author: PropTypes.shape({
+        name: PropTypes.string,
+        position: PropTypes.string,
+      }),
+    })
+  ),
+  carousel: PropTypes.bool,
+  visibleCount: PropTypes.number,
 };
--- src/components/BlockQuote/stories.jsx
+++ src/components/BlockQuote/stories.jsx
@@ -1,5 +1,56 @@
 import { BlockQuote } from "./component";
 
+const items = [
+  {
+    quote:
+      "“Lorem ipsum dolor sit amet consectetur. Scelerisque ornare euismod tortor commodo quis amet fermentum nulla. Ornare donec curabitur urna leo. Pharetra nunc et duis nec donec morbi id. Tellus netus lorem quis parturient nisi. Sit facilisi tempor a sed. Velit quis”",
+    author: {
+      name: "Name Surname",
+      position: "Position",
+    },
+  },
+  {
+    quote:
+      "“Lorem ipsum dolor sit amet consectetur. Scelerisque ornare euismod tortor commodo quis amet fermentum nulla. Ornare donec curabitur urna leo. Pharetra nunc et duis nec donec morbi id. Tellus netus lorem quis parturient nisi. Sit facilisi tempor a sed. Velit quis”",
+    author: {
+      name: "Name Surname",
+      position: "Position",
+    },
+  },
+  {
+    quote:
+      "“Lorem ipsum dolor sit amet consectetur. Scelerisque ornare euismod tortor commodo quis amet fermentum nulla. Ornare donec curabitur urna leo. Pharetra nunc et duis nec donec morbi id. Tellus netus lorem quis parturient nisi. Sit facilisi tempor a sed. Velit quis”",
+    author: {
+      name: "Name Surname",
+      position: "Position",
+    },
+  },
+  {
+    quote:
+      "“Lorem ipsum dolor sit amet consectetur. Scelerisque ornare euismod tortor commodo quis amet fermentum nulla. Ornare donec curabitur urna leo. Pharetra nunc et duis nec donec morbi id. Tellus netus lorem quis parturient nisi. Sit facilisi tempor a sed. Velit quis”",
+    author: {
+      name: "Name Surname",
+      position: "Position",
+    },
+  },
+  {
+    quote:
+      "“Lorem ipsum dolor sit amet consectetur. Scelerisque ornare euismod tortor commodo quis amet fermentum nulla. Ornare donec curabitur urna leo. Pharetra nunc et duis nec donec morbi id. Tellus netus lorem quis parturient nisi. Sit facilisi tempor a sed. Velit quis”",
+    author: {
+      name: "Name Surname",
+      position: "Position",
+    },
+  },
+  {
+    quote:
+      "“Lorem ipsum dolor sit amet consectetur. Scelerisque ornare euismod tortor commodo quis amet fermentum nulla. Ornare donec curabitur urna leo. Pharetra nunc et duis nec donec morbi id. Tellus netus lorem quis parturient nisi. Sit facilisi tempor a sed. Velit quis”",
+    author: {
+      name: "Name Surname",
+      position: "Position",
+    },
+  },
+];
+
 export default {
   title: "Components/BlockQuote",
   component: BlockQuote,
@@ -8,6 +59,38 @@ export default {
   argTypes: {},
 };
 
-// export const Default = {
-//   args: {},
-// };
+export const Carousel = {
+  args: {
+    title: "Block quote title",
+    items,
+    carousel: true,
+    visibleCount: 1,
+  },
+};
+
+export const Static = {
+  args: {
+    title: "Block quote title",
+    items: items.slice(0, 1),
+    carousel: false,
+    visibleCount: 1,
+  },
+};
+
+export const CarouselThreeCards = {
+  args: {
+    title: "Block quote title",
+    items,
+    carousel: true,
+    visibleCount: 3,
+  },
+};
+
+export const StaticThreeCards = {
+  args: {
+    title: "Block quote title",
+    items: items.slice(0, 3),
+    carousel: false,
+    visibleCount: 3,
+  },
+};
