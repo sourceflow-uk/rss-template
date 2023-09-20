a67b809c0d1b5c41ea0aaba55ba0f6d603271394
Sean Jermey <sean.jermey@sourceflow.co.uk>
2023-09-20 17:05:33 +0100

    LatestJobs component

--- src/components/Accordion/component.jsx
+++ src/components/Accordion/component.jsx
@@ -2,21 +2,24 @@ import classes from "./styles.module.scss";
 import clsx from "classnames";
 import { useState } from "react";
 import PropTypes from "prop-types";
+import { Container } from "react-bootstrap";
 
 export const Accordion = ({ className, items, defaultActiveKey }) => {
   const [active, setActive] = useState(defaultActiveKey);
 
   return (
     <div className={clsx(className, classes.accordion)}>
-      {items.map(({ title, body }, k) => (
-        <article className={clsx(classes.accordion__item, k === active && classes["accordion__item--active"])}>
-          <header onClick={() => setActive(k)} className="py-3">
-            <h4>{title}</h4>
-            <a id={`accordion-item-${k}`} />
-          </header>
-          <section dangerouslySetInnerHTML={{ __html: body }} />
-        </article>
-      ))}
+      <Container className="mw-lg">
+        {items.map(({ title, body }, k) => (
+          <article className={clsx(classes.accordion__item, k === active && classes["accordion__item--active"])}>
+            <header onClick={() => setActive(k)} className="py-3">
+              <h4>{title}</h4>
+              <a id={`accordion-item-${k}`} />
+            </header>
+            <section dangerouslySetInnerHTML={{ __html: body }} />
+          </article>
+        ))}
+      </Container>
     </div>
   );
 };
