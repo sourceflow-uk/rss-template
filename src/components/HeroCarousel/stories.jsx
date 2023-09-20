326891645e2e5c2b09d7c4ba3c73d8da5fc9f6a4
Sean Jermey <sean.jermey@sourceflow.co.uk>
2023-09-20 09:08:48 +0100

    hero carousel component

--- src/components/HeroCarousel/stories.jsx
+++ src/components/HeroCarousel/stories.jsx
@@ -8,6 +8,84 @@ export default {
   argTypes: {},
 };
 
-// export const Default = {
-//   args: {},
-// };
+export const Default = {
+  args: {
+    className: "",
+    items: [
+      {
+        title: "Hero Title",
+        img: "https://picsum.photos/1440/300?random=1",
+        description: "Hero description",
+      },
+      {
+        title: "Hero Title",
+        img: "https://picsum.photos/1440/300?random=1",
+        description: "Hero description",
+      },
+    ],
+  },
+};
+
+export const TitleOnly = {
+  args: {
+    className: "",
+    items: [
+      {
+        title: "Hero Title",
+        img: "https://picsum.photos/1440/300?random=1",
+      },
+      {
+        title: "Hero Title",
+        img: "https://picsum.photos/1440/300?random=1",
+      },
+    ],
+  },
+};
+
+export const NoControls = {
+  args: {
+    className: "",
+    items: [
+      {
+        title: "Hero Title",
+        img: "https://picsum.photos/1440/300?random=1",
+        description: "Hero description",
+      },
+      {
+        title: "Hero Title",
+        img: "https://picsum.photos/1440/300?random=1",
+        description: "Hero description",
+      },
+    ],
+    controls: false,
+  },
+};
+
+export const TitleOnlyNoControls = {
+  args: {
+    className: "",
+    items: [
+      {
+        title: "Hero Title",
+        img: "https://picsum.photos/1440/300?random=1",
+      },
+      {
+        title: "Hero Title",
+        img: "https://picsum.photos/1440/300?random=1",
+      },
+    ],
+    controls: false,
+  },
+};
+
+export const Video = {
+  args: {
+    items: [
+      {
+        img: "https://picsum.photos/1440/300?random=1",
+        video: "video_embed_url",
+      },
+    ],
+    controls: false,
+  },
+};
