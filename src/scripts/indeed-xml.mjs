07202dd2b11e55a45f084b533d9fb5fe4ca92e8f
Sean Jermey <sean.jermey@sourceflow.co.uk>
2023-10-26 11:58:13 +0100

    amend indeed script

--- src/scripts/indeed-xml.mjs
+++ src/scripts/indeed-xml.mjs
@@ -1,43 +1,42 @@
-/*
-!!! IMPORTANT !!!
+import Jobs from "@sourceflow-uk/sourceflow-sdk/jobs.js";
+import BaseObject from "@sourceflow-uk/sourceflow-sdk/base_object.js";
+import { writeFileSync } from "fs";
+import { decode } from "html-entities";
+import globalData from "../../.sourceflow/global.json" assert { type: "json" };
 
-To use this feature in another project you must do the following:
-- copy this script to `/scripts/indeed-xml.mjs`, note the special .mjs extension
-- add `/public/indeed-jobs.xml` to the .gitignore file
-- modify the `build` command in package.json:
-    - Insert `node ./scripts/indeed-xml.mjs` between `next-sitemap` and `next export`
-    - Ensure each command is separated with `&&`
-- update the values below to be correct for the current site
+/**
+ * !!! IMPORTANT !!!
+ *
+ * To use this feature in another project you must do the following:
+ * - copy this script to `/scripts/indeed-xml.mjs`, note the special .mjs extension
+ * - add `/public/indeed-jobs.xml` to the .gitignore file
+ * - modify the `build` command in package.json:
+ *     - Insert `node ./scripts/indeed-xml.mjs` between `next-sitemap` and `next export`
+ *     - Ensure each command is separated with `&&`
+ * - update the values below to be correct for the current site
+ */
+const global = new BaseObject(globalData, "en").toJson();
+const jobs = (await Jobs()).filter((job) => new Date(job?.expires_at) >= new Date()).getItems();
 
-*/
-import { Jobs } from '@sourceflow-uk/sourceflow-sdk';
-import { writeFileSync } from 'fs';
-import { decode } from 'html-entities';
+const siteName = global["_theme.company.name"];
+const domain = global["_theme.company.domain"];
+const clientEmail = global["_theme.company.email"];
 
-const jobs = (await Jobs()).filter((job) => (new Date(job?.expires_at)) >= (new Date())).getItems();
-
-// !!!!! UPDATE THESE VALUES FOR EACH SITE
-const siteName = 'Blue Arrow'; // Name of the client/website
-const domain   = 'https://www.bluearrow.co.uk'; // Full domain
-const jobsPath = `${domain}/jobs`; // Prefix for all jobs pages
-const clientEmail = ''; // An email address Indeed may provide
-// !!!!! UPDATE THESE VALUES FOR EACH SITE
-
-const indeedFeed =
-`<?xml version="1.0" encoding="utf-8"?>
+const indeedFeed = `<?xml version="1.0" encoding="utf-8"?>
 <source>
     <publisher>${siteName}</publisher>
     <publisherurl>${domain}</publisherurl>
-    ${ jobs.map((job) => {
-        const cleanLocation = job.location?.replace(", United Kingdom", "")
-        return(
-            // Modify this to add the needed data if not already supported
-            `<job>
+    ${jobs
+      .map((job) => {
+        const cleanLocation = job.location?.replace(", United Kingdom", "");
+        return (
+          // Modify this to add the needed data if not already supported
+          `<job>
                 <title><![CDATA[${job.title}]]></title>
                 <date><![CDATA[${job.created_at}]]></date>
                 <referencenumber><![CDATA[${job.external_reference}]]></referencenumber>
                 <url>
-                    <![CDATA[${`${jobsPath}/${job.url_slug}?source=Indeed`}]]>
+                    <![CDATA[${`${domain}/jobs/${job.url_slug}?source=Indeed`}]]>
                 </url>
                 <company><![CDATA[${siteName}]]></company>
                 <sourcename><![CDATA[${siteName}]]></sourcename>
@@ -52,13 +51,10 @@ const indeedFeed =
                 <salary><![CDATA[${job.salary_package} per year]]></salary>
                 <expirationdate><![CDATA[${job.expires_at}]]></expirationdate>
             </job>`
-        )
-
-        })
-        .join('')
-    }
+        );
+      })
+      .join("")}
 </source>`;
 
 // writes the file as a static xml file in the public folder
-writeFileSync('public/indeed-jobs.xml', indeedFeed);
-
+writeFileSync("public/indeed-jobs.xml", indeedFeed);
