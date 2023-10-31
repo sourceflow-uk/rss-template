a3e998506923dfca8f699c9b1ade9f335936d5bb
Sean Jermey <sean.jermey@sourceflow.co.uk>
2023-10-31 12:21:56 +0000

    migrate asset to gallery

--- src/ui/SiteFooter/index.jsx
+++ src/ui/SiteFooter/index.jsx
@@ -3,16 +3,14 @@ import { Container, Stack } from "react-bootstrap";
 import { SiteFooterCopyright, SiteFooterDetails, SiteFooterNav, SiteFooterSocials } from "./__components";
 import classes from "./styles.module.scss";
 import PropTypes from "prop-types";
-import SourceFlowImage from "@sourceflow-uk/sourceflowimage";
 import { getRoute } from "@/getters/getRoute";
-import { getAsset } from "@/getters/getAsset";
 import { getGlobal } from "@/getters/getGlobal";
+import { Image } from "@/ui";
 
 export default function SiteFooter({ className }) {
   const global = getGlobal();
   const company_name = global["_theme.company.name"];
-  const company_logo = getAsset("_theme.company.logo.white");
-  const footer_bg = getAsset("_theme.footer.bg");
+  const footer_bg = global["_theme.footer.bg"];
 
   return (
     <div className={clsx(className, classes.footer, "pt-5 pb-4")} style={{ backgroundImage: `url(${footer_bg})` }}>
@@ -29,7 +27,7 @@ export default function SiteFooter({ className }) {
         <Stack className="flex-lg-row justify-content-between align-items-center" gap={3}>
           <SiteFooterDetails className={clsx(classes.footer__details, "flex-lg-row")} />
           <a className={classes.footer__brand} href={getRoute("home")}>
-            <SourceFlowImage src={company_logo} size="260x44" alt={company_name} />
+            <Image img={{ path: "_theme.company.logo.white" }} size="260x44" alt={company_name} />
           </a>
         </Stack>
         <SiteFooterCopyright className={clsx(classes.footer__copyright, "flex-lg-row mt-5 small")} />
