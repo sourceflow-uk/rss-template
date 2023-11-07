2698fec73339fdb20382fa1c778b8b1d0bd7df7c
Sean Jermey <sean.jermey@sourceflow.co.uk>
2023-11-07 10:20:38 +0000

    add reciteme script

--- src/pages/_document.page.js
+++ src/pages/_document.page.js
@@ -6,6 +6,7 @@ import { hexToRgb } from "@/functions/hexToRgb";
 export default function Document() {
   const global = getGlobal();
   const gtag = global["_theme.google.gtag"];
+  const reciteme = global["_theme.reciteme.key"];
 
   return (
     <Html lang="en">
@@ -19,6 +20,16 @@ export default function Document() {
                 function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
                 gtag('config', '${gtag}');
+              `}
+            </Script>
+            <Script id="reciteme">
+              {`
+                var serviceUrl = "//api.reciteme.com/asset/js?key=";
+                var serviceKey = "${reciteme}";
+                var options = {};  // Options can be added as needed
+                var autoLoad = false;
+                var enableFragment = "#reciteEnable";
+                var loaded = [], frag = !1; window.location.hash === enableFragment && (frag = !0); function loadScript(c, b) { var a = document.createElement("script"); a.type = "text/javascript"; a.readyState ? a.onreadystatechange = function () { if ("loaded" == a.readyState || "complete" == a.readyState) a.onreadystatechange = null, void 0 != b && b() } : void 0 != b && (a.onload = function () { b() }); a.src = c; document.getElementsByTagName("head")[0].appendChild(a) } function _rc(c) { c += "="; for (var b = document.cookie.split(";"), a = 0; a < b.length; a++) { for (var d = b[a]; " " == d.charAt(0);)d = d.substring(1, d.length); if (0 == d.indexOf(c)) return d.substring(c.length, d.length) } return null } function loadService(c) { for (var b = serviceUrl + serviceKey, a = 0; a < loaded.length; a++)if (loaded[a] == b) return; loaded.push(b); loadScript(serviceUrl + serviceKey, function () { "function" === typeof _reciteLoaded && _reciteLoaded(); "function" == typeof c && c(); Recite.load(options); Recite.Event.subscribe("Recite:load", function () { Recite.enable() }) }) } "true" == _rc("Recite.Persist") && loadService(); if (autoLoad && "false" != _rc("Recite.Persist") || frag) document.addEventListener ? document.addEventListener("DOMContentLoaded", function (c) { loadService() }) : loadService();
               `}
             </Script>
           </>
