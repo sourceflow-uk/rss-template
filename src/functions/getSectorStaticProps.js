f20139bfe970d1e329d6fe05590d869843e6c0dd
Sean Jermey <sean.jermey@sourceflow.co.uk>
2023-11-23 12:17:48 +0000

    amend

--- src/functions/getSectorStaticProps.js
+++ src/functions/getSectorStaticProps.js
@@ -32,9 +32,7 @@ export const getSectorStaticProps = ({ sector_id, pages_helper = null } = {}) =>
           placeholder:
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
         },
-        img: {
-          path: `page.${sector.url_slug}.component.Header.img`,
-        },
+        img: sector.cover_image ?? null,
         back: {
           path: `page.home.component.Header.back`,
           placeholder: `Back to Home`,
@@ -45,6 +43,7 @@ export const getSectorStaticProps = ({ sector_id, pages_helper = null } = {}) =>
     {
       component: "RichText",
       props: {
+        className: "bg-primary text-white py-4 py-md-5",
         body: {
           path: `page.${sector.url_slug}.component.Intro.body`,
           placeholder: "",
