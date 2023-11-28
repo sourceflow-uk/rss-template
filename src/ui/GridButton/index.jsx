ece154f3db89d0e9d613f3e7865e1991547cb524
Sean Jermey <sean.jermey@sourceflow.co.uk>
2023-11-28 16:09:20 +0000

    amend Grid button

--- src/ui/GridButton/index.jsx
+++ src/ui/GridButton/index.jsx
@@ -2,13 +2,14 @@ import classes from "./styles.module.scss";
 import clsx from "classnames";
 import PropTypes from "prop-types";
 import Image from "../Image";
+import { Title } from "@/ui";
 
 export default function GridButton({ className, title, img, href }) {
   return (
     <a className={clsx(className, classes.button)} href={href}>
-      <Image className={classes.button__img} img={img} size="160x160" alt={title} />
+      <Image img={img} size="100x100" alt={title} />
       <div className={classes.button__body}>
-        <h5 className="p-4 mx-auto mb-0">{title}</h5>
+        <Title className="mt-3 mx-auto mb-0" tag="h3" title={title} />
       </div>
     </a>
   );
