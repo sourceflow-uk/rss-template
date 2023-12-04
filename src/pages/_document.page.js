427f233587fa314d5f2cfd2831b60817b761d2b3
Sean Jermey <sean.jermey@sourceflow.co.uk>
2023-12-04 11:37:56 +0000

    add google-site-verification

--- src/pages/_document.page.js
+++ src/pages/_document.page.js
@@ -9,6 +9,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta name="google-site-verification" content="AWapnNPK2OndyLbQa9vw6Q2NuF0sIWUWl2WigEzrbuQ" />
         <Script id="gtm" strategy="beforeInteractive">
           {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-W9BK8R');`}
         </Script>
