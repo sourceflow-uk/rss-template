7dc0bb682d27e3f5099d5cfd5571a8444edf71bd
Sean Jermey <sean.jermey@sourceflow.co.uk>
2024-01-22 16:49:39 +0000

    Support ticket #2519

--- src/ui/Content/index.jsx
+++ src/ui/Content/index.jsx
@@ -1,6 +1,7 @@
 import * as components from "@/components";
 import { logo_carousel_helper } from "@/helpers/logo_carousel_helper";
 import { useRouter } from "next/router";
+import { FadeIn } from "react-slide-fade-in";
 
 export default function Content({ items, additionalComponents }) {
   const router = useRouter();
@@ -24,7 +25,7 @@ export default function Content({ items, additionalComponents }) {
       {items
         .filter((i) => !!i)
         .map(({ component, id, props }, k) => (
-          <div key={k}>
+          <FadeIn from="bottom" delayInMilliseconds={150} key={k}>
             <a id={id ?? component} />
             {(() => {
               if (typeof component !== "undefined" && component in allowedComponents) {
@@ -33,7 +34,7 @@ export default function Content({ items, additionalComponents }) {
                 return <Component {...props} />;
               }
             })()}
-          </div>
+          </FadeIn>
         ))}
     </section>
   );
